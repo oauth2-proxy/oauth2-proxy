c1116ea5068f7afa97c45a20c8c3e98aafd4031d
Jehiah Czebotar <jehiah@gmail.com>
2017-03-28 23:09:53 -0400

    travis: run gofmt and go vet

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "gofmt"
+diff -u <(echo -n) <(gofmt -d $(find . -type f -name '*.go' -not -path "./.godeps/*"))
+echo "go vet"
+go vet ./...
+echo "go test"
 go test -timeout 60s ./...
+echo "go test -race"
 GOMAXPROCS=4 go test -timeout 60s -race ./...
