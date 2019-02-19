78410d2ff079b39647b2add09927abde36052692
YAEGASHI Takeshi <yaegashi@debian.org>
2019-02-20 01:33:19 +0900

    Store only email and user in cookie when cipher is unavailable
    This improves safety and robustness, and also preserves the existing
    behaviour.

--- providers/session_state.go
+++ providers/session_state.go
@@ -47,24 +47,31 @@ func (s *SessionState) String() string {
 
 // EncodeSessionState returns string representation of the current session
 func (s *SessionState) EncodeSessionState(c *cookie.Cipher) (string, error) {
-	ss := *s
+	var ss SessionState
 	if c == nil {
-		ss.AccessToken = ""
-		ss.IDToken = ""
-		ss.RefreshToken = ""
+		// Store only Email and User when cipher is unavailable
+		ss.Email = s.Email
+		ss.User = s.User
 	} else {
+		ss = *s
 		var err error
-		ss.AccessToken, err = c.Encrypt(ss.AccessToken)
-		if err != nil {
-			return "", err
+		if ss.AccessToken != "" {
+			ss.AccessToken, err = c.Encrypt(ss.AccessToken)
+			if err != nil {
+				return "", err
+			}
 		}
-		ss.IDToken, err = c.Encrypt(ss.IDToken)
-		if err != nil {
-			return "", err
+		if ss.IDToken != "" {
+			ss.IDToken, err = c.Encrypt(ss.IDToken)
+			if err != nil {
+				return "", err
+			}
 		}
-		ss.RefreshToken, err = c.Encrypt(ss.RefreshToken)
-		if err != nil {
-			return "", err
+		if ss.RefreshToken != "" {
+			ss.RefreshToken, err = c.Encrypt(ss.RefreshToken)
+			if err != nil {
+				return "", err
+			}
 		}
 	}
 	b, err := json.Marshal(ss)
@@ -73,27 +80,39 @@ func (s *SessionState) EncodeSessionState(c *cookie.Cipher) (string, error) {
 
 // DecodeSessionState decodes the session cookie string into a SessionState
 func DecodeSessionState(v string, c *cookie.Cipher) (*SessionState, error) {
-	ss := &SessionState{}
-	err := json.Unmarshal([]byte(v), ss)
+	var ss SessionState
+	var s *SessionState
+	err := json.Unmarshal([]byte(v), &s)
 	if err != nil {
 		return nil, err
 	}
-	if c != nil {
-		ss.AccessToken, err = c.Decrypt(ss.AccessToken)
-		if err != nil {
-			return nil, err
+	if c == nil {
+		// Load only Email and User when cipher is unavailable
+		ss.Email = s.Email
+		ss.User = s.User
+	} else {
+		ss = *s
+		if ss.AccessToken != "" {
+			ss.AccessToken, err = c.Decrypt(ss.AccessToken)
+			if err != nil {
+				return nil, err
+			}
 		}
-		ss.IDToken, err = c.Decrypt(ss.IDToken)
-		if err != nil {
-			return nil, err
+		if ss.IDToken != "" {
+			ss.IDToken, err = c.Decrypt(ss.IDToken)
+			if err != nil {
+				return nil, err
+			}
 		}
-		ss.RefreshToken, err = c.Decrypt(ss.RefreshToken)
-		if err != nil {
-			return nil, err
+		if ss.RefreshToken != "" {
+			ss.RefreshToken, err = c.Decrypt(ss.RefreshToken)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 	if ss.User == "" {
 		ss.User = strings.Split(ss.Email, "@")[0]
 	}
-	return ss, nil
+	return &ss, nil
 }
