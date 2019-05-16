130d03758d0cdb8be52d792174c4c15503ed48b7
Joel Speed <Joel.speed@hotmail.co.uk>
2019-05-16 17:03:38 +0100

    Fix comments on Redis options

--- pkg/apis/options/sessions.go
+++ pkg/apis/options/sessions.go
@@ -19,11 +19,11 @@ var CookieSessionStoreType = "cookie"
 // CookieStoreOptions contains configuration options for the CookieSessionStore.
 type CookieStoreOptions struct{}
 
-// RedisSessionStoreType is used to indicate the CookieSessionStore should be
+// RedisSessionStoreType is used to indicate the RedisSessionStore should be
 // used for storing sessions.
 var RedisSessionStoreType = "redis"
 
-// RedisStoreOptions contains configuration options for the CookieSessionStore.
+// RedisStoreOptions contains configuration options for the RedisSessionStore.
 type RedisStoreOptions struct {
 	RedisConnectionURL string `flag:"redis-connection-url" cfg:"redis_connection_url" env:"OAUTH2_PROXY_REDIS_CONNECTION_URL"`
 }
