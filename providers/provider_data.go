6346d269181814bfeaceb8f821e7c4e4bea2baeb
Pavel Kirichenko <pasha-r@standalone.name>
2020-01-16 19:01:49 +0100

    added ClientSecretFile in ProviderData

--- providers/provider_data.go
+++ providers/provider_data.go
@@ -1,6 +1,9 @@
 package providers
 
 import (
+	"errors"
+	"github.com/pusher/oauth2_proxy/pkg/logger"
+	"io/ioutil"
 	"net/url"
 )
 
@@ -9,7 +12,8 @@ import (
 type ProviderData struct {
 	ProviderName      string
 	ClientID          string
-	ClientSecret      string
+	ClientSecret_     string
+	ClientSecretFile  string
 	LoginURL          *url.URL
 	RedeemURL         *url.URL
 	ProfileURL        *url.URL
@@ -21,3 +25,17 @@ type ProviderData struct {
 
 // Data returns the ProviderData
 func (p *ProviderData) Data() *ProviderData { return p }
+
+func (p *ProviderData) ClientSecret() (ClientSecret string, err error) {
+	if p.ClientSecret_ != "" || p.ClientSecretFile == "" {
+		return p.ClientSecret_, nil
+	}
+
+	// Getting ClientSecret can fail in runtime so we need to report it without returning the file name to the user
+	ClientSecret_, err := ioutil.ReadFile(p.ClientSecretFile)
+	if err != nil {
+		logger.Printf("error reading client secret file %s: %s", p.ClientSecretFile, err)
+		return "", errors.New("could not read client secret file")
+	}
+	return string(ClientSecret_), nil
+}
