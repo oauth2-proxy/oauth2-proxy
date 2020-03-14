0d40cfe910407bd9e47fa643d35909835ed859d2
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2020-03-14 15:51:59 +0900

    fix test to support go1.14 and go1.13

--- options_test.go
+++ options_test.go
@@ -156,13 +156,7 @@ func TestProxyURLsError(t *testing.T) {
 	o.Upstreams = append(o.Upstreams, "127.0.0.1:8081")
 	err := o.Validate()
 	assert.NotEqual(t, nil, err)
-
-	// The error message for net/url url.Parse has changed slightly since go1.14.
-	// https: //github.com/golang/go/commit/64cfe9fe22113cd6bc05a2c5d0cbe872b1b57860
-	expected := errorMsg([]string{
-		"error parsing upstream: parse \"127.0.0.1:8081\": " +
-			"first path segment in URL cannot contain colon"})
-	assert.Equal(t, expected, err.Error())
+	assert.Contains(t, err.Error(), "error parsing upstream")
 }
 
 func TestCompiledRegex(t *testing.T) {
