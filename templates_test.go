308a0fc1a676bdf1a9b92ea20481d0063ff73812
n-i-x <440158+n-i-x@users.noreply.github.com>
2020-05-06 23:58:48 +0000

    Add template tests for new template functions

--- templates_test.go
+++ templates_test.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestLoadTemplates(t *testing.T) {
+	dir, err := ioutil.TempDir("", "templatetest")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	templateHTML := `{{.TestString}} {{.TestString | ToLower}} {{.TestString | ToUpper}}`
+	signInFile := filepath.Join(dir, "sign_in.html")
+	if err := ioutil.WriteFile(signInFile, []byte(templateHTML), 0666); err != nil {
+		log.Fatal(err)
+	}
+	errorFile := filepath.Join(dir, "error.html")
+	if err := ioutil.WriteFile(errorFile, []byte(templateHTML), 0666); err != nil {
+		log.Fatal(err)
+	}
+	templates := loadTemplates(dir)
+	assert.NotEqual(t, templates, nil)
+
+	data := struct {
+		TestString string
+	}{
+		TestString: "Testing",
+	}
+
+	var sitpl bytes.Buffer
+	templates.ExecuteTemplate(&sitpl, "sign_in.html", data)
+	assert.Equal(t, "Testing testing TESTING", sitpl.String())
+
+	var errtpl bytes.Buffer
+	templates.ExecuteTemplate(&errtpl, "error.html", data)
+	assert.Equal(t, "Testing testing TESTING", errtpl.String())
+}
 
 func TestTemplatesCompile(t *testing.T) {
 	templates := getTemplates()
