59da013ad77ce5967823d0642e1d71e8daa10c55
Tero Pihlaja <tero.pihlaja@turckvilant.com>
2020-05-27 14:02:29 +0300

    Add flagSet for proxy timeout

--- pkg/apis/options/options.go
+++ pkg/apis/options/options.go
@@ -245,6 +245,7 @@ func NewFlagSet() *pflag.FlagSet {
 	flagSet.Bool("ssl-insecure-skip-verify", false, "skip validation of certificates presented when using HTTPS providers")
 	flagSet.Bool("ssl-upstream-insecure-skip-verify", false, "skip validation of certificates presented when using HTTPS upstreams")
 	flagSet.Duration("flush-interval", time.Duration(1)*time.Second, "period between response flushing when streaming responses")
+	flagSet.Duration("proxy-time-out", time.Duration(30)*time.Second, "timeout for upstream proxy")
 	flagSet.Bool("skip-jwt-bearer-tokens", false, "will skip requests that have verified JWT bearer tokens (default false)")
 	flagSet.StringSlice("extra-jwt-issuers", []string{}, "if skip-jwt-bearer-tokens is set, a list of extra JWT issuer=audience pairs (where the issuer URL has a .well-known/openid-configuration or a .well-known/jwks.json)")
 
