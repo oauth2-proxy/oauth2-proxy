7dae547ce6d50b274e3b77ffa764f69643353e7d
Joel Speed <joel.speed@hotmail.co.uk>
2020-06-14 16:42:05 +0100

    Add HealthCheck middleware

---
+++ pkg/middleware/middleware_suite_test.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"testing"
+
+	"github.com/oauth2-proxy/oauth2-proxy/pkg/logger"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestMiddlewareSuite(t *testing.T) {
+	logger.SetOutput(GinkgoWriter)
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Middleware")
+}
