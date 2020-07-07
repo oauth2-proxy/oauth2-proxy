e4faeb27490babd5189648aa087b2050f2595303
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2020-07-07 17:46:28 +0900

    Use singleflight to prevent calling duplicate Redis command

--- pkg/sessions/redis/client.go
+++ pkg/sessions/redis/client.go
@@ -2,9 +2,11 @@ package redis
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/go-redis/redis/v7"
+	"golang.org/x/sync/singleflight"
 )
 
 // Client is wrapper interface for redis.Client and redis.ClusterClient.
@@ -18,6 +20,8 @@ var _ Client = (*client)(nil)
 
 type client struct {
 	*redis.Client
+
+	group singleflight.Group
 }
 
 func newClient(c *redis.Client) Client {
@@ -25,21 +29,44 @@ func newClient(c *redis.Client) Client {
 }
 
 func (c *client) Get(ctx context.Context, key string) ([]byte, error) {
-	return c.WithContext(ctx).Get(key).Bytes()
+	v, err, _ := c.group.Do(key, func() (interface{}, error) {
+		b, err := c.WithContext(ctx).Get(key).Bytes()
+		if err != nil {
+			return nil, err
+		}
+		return b, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	b, ok := v.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("invalid value type: key=%s, value%v", key, v)
+	}
+	return b, nil
 }
 
 func (c *client) Set(ctx context.Context, key string, value []byte, expiration time.Duration) error {
-	return c.WithContext(ctx).Set(key, value, expiration).Err()
+	_, err, _ := c.group.Do(key, func() (interface{}, error) {
+		return nil, c.WithContext(ctx).Set(key, value, expiration).Err()
+	})
+	return err
 }
 
 func (c *client) Del(ctx context.Context, key string) error {
-	return c.WithContext(ctx).Del(key).Err()
+	_, err, _ := c.group.Do(key, func() (interface{}, error) {
+		return nil, c.WithContext(ctx).Del(key).Err()
+	})
+	return err
 }
 
 var _ Client = (*clusterClient)(nil)
 
 type clusterClient struct {
 	*redis.ClusterClient
+
+	group singleflight.Group
 }
 
 func newClusterClient(c *redis.ClusterClient) Client {
@@ -47,13 +74,34 @@ func newClusterClient(c *redis.ClusterClient) Client {
 }
 
 func (c *clusterClient) Get(ctx context.Context, key string) ([]byte, error) {
-	return c.WithContext(ctx).Get(key).Bytes()
+	v, err, _ := c.group.Do(key, func() (interface{}, error) {
+		b, err := c.WithContext(ctx).Get(key).Bytes()
+		if err != nil {
+			return nil, err
+		}
+		return b, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	b, ok := v.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("invalid value type: key=%s, value%v", key, v)
+	}
+	return b, nil
 }
 
 func (c *clusterClient) Set(ctx context.Context, key string, value []byte, expiration time.Duration) error {
-	return c.WithContext(ctx).Set(key, value, expiration).Err()
+	_, err, _ := c.group.Do(key, func() (interface{}, error) {
+		return nil, c.WithContext(ctx).Set(key, value, expiration).Err()
+	})
+	return err
 }
 
 func (c *clusterClient) Del(ctx context.Context, key string) error {
-	return c.WithContext(ctx).Del(key).Err()
+	_, err, _ := c.group.Do(key, func() (interface{}, error) {
+		return nil, c.WithContext(ctx).Del(key).Err()
+	})
+	return err
 }
