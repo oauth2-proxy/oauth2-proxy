cf704935cc6f839336acb266862e0ba9e757071a
Isabelle Erin COWAN-BERGMAN <isabelle.cowan-bergman@doctolib.com>
2020-07-10 17:29:12 +0200

    fix

--- oauthproxy.go
+++ oauthproxy.go
@@ -309,7 +309,7 @@ func NewOAuthProxy(opts *options.Options, validator func(string) bool) (*OAuthPr
 		if ipNet := ip.ParseIPNet(ipStr); ipNet != nil {
 			trustedIPs.AddIPNet(*ipNet)
 		} else {
-			return nil, fmt.Errorf("Could not parse IP network (%s)", ipStr)
+			return nil, fmt.Errorf("could not parse IP network (%s)", ipStr)
 		}
 	}
 
