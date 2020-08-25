b0b29bbe946eba2c07150d36c26416460da3e47f
Dan Bond <danbond@protonmail.com>
2020-08-25 10:51:49 +0100

    dist.sh: remove go version from asset links

--- dist.sh
+++ dist.sh
@@ -20,7 +20,7 @@ mkdir -p release
 
 # Create architecture specific release dirs
 for ARCH in "${ARCHS[@]}"; do
-	mkdir -p release/${BINARY}-${VERSION}.${ARCH}.${GO_VERSION}
+	mkdir -p release/${BINARY}-${VERSION}.${ARCH}
 
 	GO_OS=$(echo $ARCH | awk -F- '{print $1}')
 	GO_ARCH=$(echo $ARCH | awk -F- '{print $2}')
@@ -28,19 +28,19 @@ for ARCH in "${ARCHS[@]}"; do
 	# Create architecture specific binaries
 	if [[ ${GO_ARCH} == "armv6" ]]; then
 		GO111MODULE=on GOOS=${GO_OS} GOARCH=arm GOARM=6 CGO_ENABLED=0 go build -ldflags="-X main.VERSION=${VERSION}" \
-			-o release/${BINARY}-${VERSION}.${ARCH}.${GO_VERSION}/${BINARY} github.com/oauth2-proxy/oauth2-proxy
+			-o release/${BINARY}-${VERSION}.${ARCH}/${BINARY} github.com/oauth2-proxy/oauth2-proxy
 	else
 		GO111MODULE=on GOOS=${GO_OS} GOARCH=${GO_ARCH} CGO_ENABLED=0 go build -ldflags="-X main.VERSION=${VERSION}" \
-			-o release/${BINARY}-${VERSION}.${ARCH}.${GO_VERSION}/${BINARY} github.com/oauth2-proxy/oauth2-proxy
+			-o release/${BINARY}-${VERSION}.${ARCH}/${BINARY} github.com/oauth2-proxy/oauth2-proxy
 	fi
 
 	cd release
 
 	# Create sha256sum for architecture specific binary
-	sha256sum ${BINARY}-${VERSION}.${ARCH}.${GO_VERSION}/${BINARY} > ${BINARY}-${VERSION}.${ARCH}-sha256sum.txt
+	sha256sum ${BINARY}-${VERSION}.${ARCH}/${BINARY} > ${BINARY}-${VERSION}.${ARCH}-sha256sum.txt
 
 	# Create tar file for architecture specific binary
-	tar -czvf ${BINARY}-${VERSION}.${ARCH}.${GO_VERSION}.tar.gz ${BINARY}-${VERSION}.${ARCH}.${GO_VERSION}
+	tar -czvf ${BINARY}-${VERSION}.${ARCH}.tar.gz ${BINARY}-${VERSION}.${ARCH}
 
 	cd ..
 done
