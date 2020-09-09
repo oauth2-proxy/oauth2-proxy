9001fc39b0512f149529af0c85b6225057444aad
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-09-09 17:27:00 -0500

    ci: correct error handling on test script

--- test.sh
+++ test.sh
@@ -3,6 +3,13 @@
 set +e
 ./cc-test-reporter before-build
 make test
-STATUS=$?
-./cc-test-reporter after-build --exit-code $STATUS -t gocov
-exit $STATUS
+TEST_STATUS=$?
+./cc-test-reporter after-build --exit-code $TEST_STATUS -t gocov
+REPORT_STATUS=$?
+if [ "$TEST_STATUS" -ne 0 ]; then
+  echo "test failed, status code: $TEST_STATUS"
+  exit $TEST_STATUS
+elif [ "$REPORT_STATUS" -ne 0 ]; then
+  echo "after-build failed, status code: $REPORT_STATUS"
+  exit $REPORT_STATUS
+fi
