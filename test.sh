d65aa877090e77f2a504e14ff1553a498db6c5b3
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-09-20 14:43:59 -0500

    ci: more verbose test script

--- test.sh
+++ test.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # manually exiting from script, because after-build needs to run always
 set +e
+echo "1. Running before-build"
 ./cc-test-reporter before-build
+
+echo "2. Running test"
 make test
 TEST_STATUS=$?
+echo "TEST_STATUS = ${TEST_STATUS}"
+
+echo "3. Running after-build"
 ./cc-test-reporter after-build --exit-code $TEST_STATUS -t gocov
 REPORT_STATUS=$?
+echo "REPORT_STATUS = ${REPORT_STATUS}"
+
 if [ "$TEST_STATUS" -ne 0 ]; then
   echo "test failed, status code: $TEST_STATUS"
   exit $TEST_STATUS
