ac5417a4ec6cdfcad8267e19990b0cbf499babf9
Scott Guymer <scott.guymer@philips.com>
2020-09-23 18:04:05 +0200

    Unable to redirect to url with a url in the query string
    When redirecting to the final destination if the URL had a URL(like) value in the query string it would fail and redirect to /
    
    To fix this i corrected the regex to anchor at the start of the string being checked so it does not detect the URL in the query string and fail the test.

--- oauthproxy_test.go
+++ oauthproxy_test.go
@@ -297,6 +297,16 @@ func TestIsValidRedirect(t *testing.T) {
 			Redirect:       "/\t/\t\\evil.com",
 			ExpectedResult: false,
 		},
+		{
+			Desc:           "validHTTPwithQueryIncURL",
+			Redirect:       "https://foo.bar/redirect?param=https://evil.com/path|12345",
+			ExpectedResult: true,
+		},
+		{
+			Desc:           "validSingleSlashWithQueryIncURL",
+			Redirect:       "/redirect?param=https://evil.com/path|12345",
+			ExpectedResult: true,
+		},
 	}
 
 	for _, tc := range testCases {
