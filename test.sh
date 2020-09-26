3fe84a74769c6d47aef8e920be47f414ce4e63c5
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-09-26 15:05:38 -0500

    ci: configure CC_TEST_REPORTER_ID env

--- test.sh
+++ test.sh
@@ -7,12 +7,14 @@ echo "1. Running before-build"
 echo "2. Running test"
 make test
 TEST_STATUS=$?
-echo "TEST_STATUS = ${TEST_STATUS}"
+echo "TEST_STATUS: ${TEST_STATUS}"
+
+echo "CC_TEST_REPORTER_ID: ${CC_TEST_REPORTER_ID}"
 
 echo "3. Running after-build"
 ./cc-test-reporter after-build --exit-code $TEST_STATUS -t gocov
 REPORT_STATUS=$?
-echo "REPORT_STATUS = ${REPORT_STATUS}"
+echo "REPORT_STATUS: ${REPORT_STATUS}"
 
 if [ "$TEST_STATUS" -ne 0 ]; then
   echo "test failed, status code: $TEST_STATUS"
