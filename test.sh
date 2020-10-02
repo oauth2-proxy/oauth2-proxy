e4a1417973e535c475d27e3bed854ce91ea60f3e
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-10-02 14:39:08 -0500

    ci: check existence of CC_TEST_REPORT_ID variable, skip if unset

--- test.sh
+++ test.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 # manually exiting from script, because after-build needs to run always
 set +e
-echo "1. Running before-build"
-./cc-test-reporter before-build
+
+if [ -z $CC_TEST_REPORT_ID ]; then
+  echo "1. CC_TEST_REPORT_ID is unset, skipping"
+else
+  echo "1. Running before-build"
+  ./cc-test-reporter before-build
+fi
 
 echo "2. Running test"
 make test
 TEST_STATUS=$?
 echo "TEST_STATUS: ${TEST_STATUS}"
 
-echo "CC_TEST_REPORTER_ID: ${CC_TEST_REPORTER_ID}"
+if [ -z $CC_TEST_REPORT_ID ]; then
+  echo "3. CC_TEST_REPORT_ID is unset, skipping"
+else
+  echo "3. Running after-build"
+  ./cc-test-reporter after-build --exit-code $TEST_STATUS -t gocov
+fi
 
-echo "3. Running after-build"
-./cc-test-reporter after-build --exit-code $TEST_STATUS -t gocov
 REPORT_STATUS=$?
 echo "REPORT_STATUS: ${REPORT_STATUS}"
 
 if [ "$TEST_STATUS" -ne 0 ]; then
-  echo "test failed, status code: $TEST_STATUS"
+  echo "Test failed, status code: $TEST_STATUS"
   exit $TEST_STATUS
-elif [ "$REPORT_STATUS" -ne 0 ]; then
-  echo "after-build failed, status code: $REPORT_STATUS"
-  exit $REPORT_STATUS
 fi
