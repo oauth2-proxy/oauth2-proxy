0e44b0de765a9d5d32cfc91c4bb12eef67913d4a
Kevin Kreitner <kevin.kreitner@real-digital.de>
2021-03-14 11:47:44 +0100

    Fix imports

--- pkg/sessions/persistence/interfaces.go
+++ pkg/sessions/persistence/interfaces.go
@@ -2,8 +2,9 @@ package persistence
 
 import (
 	"context"
-	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
 	"time"
+
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
 )
 
 // Store is used for persistent session stores (IE not Cookie)
