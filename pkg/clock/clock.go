f9b9b40e1e96707fc27413f912267d08b86039f2
Nick Meves <nick.meves@greenhouse.io>
2021-03-27 16:53:58 -0700

    Make Clock a struct so it doesn't need initialization

--- pkg/clock/clock.go
+++ pkg/clock/clock.go
@@ -53,34 +53,13 @@ func Reset() {
 //
 // If nothing is stubbed, it defaults to default time behavior in the time
 // package.
-type Clock interface {
-	After(time.Duration) <-chan time.Time
-	AfterFunc(time.Duration, func()) *clockapi.Timer
-	Now() time.Time
-	Since(time.Time) time.Duration
-	Sleep(time.Duration)
-	Tick(time.Duration) <-chan time.Time
-	Ticker(time.Duration) *clockapi.Ticker
-	Timer(time.Duration) *clockapi.Timer
-
-	Set(time.Time)
-	Add(time.Duration) error
-	Reset()
-}
-
-type clock struct {
+type Clock struct {
 	mock *clockapi.Mock
-	sync.Mutex
-}
-
-// New returns a Clock that defaults to generic `time` functionality.
-// It can be stubbed locally or fall back to package-level clock mocks.
-func New() Clock {
-	return &clock{}
+	sync.RWMutex
 }
 
 // Set sets the Clock to a clock.Mock at the given time.Time
-func (c *clock) Set(t time.Time) {
+func (c *Clock) Set(t time.Time) {
 	c.Lock()
 	defer c.Unlock()
 	if c.mock == nil {
@@ -91,7 +70,7 @@ func (c *clock) Set(t time.Time) {
 
 // Add moves clock forward time.Duration if it is mocked. It will error
 // if the clock is not mocked.
-func (c *clock) Add(d time.Duration) error {
+func (c *Clock) Add(d time.Duration) error {
 	c.Lock()
 	defer c.Unlock()
 	if c.mock == nil {
@@ -102,65 +81,81 @@ func (c *clock) Add(d time.Duration) error {
 }
 
 // Reset removes local clock.Mock
-func (c *clock) Reset() {
+func (c *Clock) Reset() {
 	c.Lock()
 	defer c.Unlock()
 	c.mock = nil
 }
 
-func (c *clock) After(d time.Duration) <-chan time.Time {
+func (c *Clock) After(d time.Duration) <-chan time.Time {
 	if c.mock == nil {
 		return globalClock.After(d)
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.After(d)
 }
 
-func (c *clock) AfterFunc(d time.Duration, f func()) *clockapi.Timer {
+func (c *Clock) AfterFunc(d time.Duration, f func()) *clockapi.Timer {
 	if c.mock == nil {
 		return globalClock.AfterFunc(d, f)
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.AfterFunc(d, f)
 }
 
-func (c *clock) Now() time.Time {
+func (c *Clock) Now() time.Time {
 	if c.mock == nil {
 		return globalClock.Now()
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.Now()
 }
 
-func (c *clock) Since(t time.Time) time.Duration {
+func (c *Clock) Since(t time.Time) time.Duration {
 	if c.mock == nil {
 		return globalClock.Since(t)
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.Since(t)
 }
 
-func (c *clock) Sleep(d time.Duration) {
+func (c *Clock) Sleep(d time.Duration) {
 	if c.mock == nil {
 		globalClock.Sleep(d)
 		return
 	}
+	c.RLock()
+	defer c.RUnlock()
 	c.mock.Sleep(d)
 }
 
-func (c *clock) Tick(d time.Duration) <-chan time.Time {
+func (c *Clock) Tick(d time.Duration) <-chan time.Time {
 	if c.mock == nil {
 		return globalClock.Tick(d)
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.Tick(d)
 }
 
-func (c *clock) Ticker(d time.Duration) *clockapi.Ticker {
+func (c *Clock) Ticker(d time.Duration) *clockapi.Ticker {
 	if c.mock == nil {
 		return globalClock.Ticker(d)
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.Ticker(d)
 }
 
-func (c *clock) Timer(d time.Duration) *clockapi.Timer {
+func (c *Clock) Timer(d time.Duration) *clockapi.Timer {
 	if c.mock == nil {
 		return globalClock.Timer(d)
 	}
+	c.RLock()
+	defer c.RUnlock()
 	return c.mock.Timer(d)
 }
