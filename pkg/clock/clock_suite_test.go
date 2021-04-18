c569cfd8cbbabf574cd4eeaca943da96d3c4364c
Nick Meves <nick.meves@greenhouse.io>
2021-04-18 10:25:57 -0700

    Add a clock package for better time mocking (#1136)
    * Add a clock package for better time mocking
    
    * Make Clock a struct so it doesn't need initialization
    
    * Test clock package
    
    * Use atomic for live time tests
    
    * Refer to same clock.Mock throughout methods

---
+++ pkg/clock/clock_suite_test.go
@@ -0,0 +1,17 @@
+package clock_test
+
+import (
+	"testing"
+
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/logger"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestClockSuite(t *testing.T) {
+	logger.SetOutput(GinkgoWriter)
+	logger.SetErrOutput(GinkgoWriter)
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Clock")
+}
