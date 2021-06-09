525800f94fe18b3330fb85bdb3b8a8ed5a12cf57
Rishi Kambil <rishikambil@gmail.com>
2021-06-09 09:56:11 +0530

    added comments for flattenheaders fix
    Co-authored-by: Joel Speed <Joel.speed@hotmail.co.uk>

--- pkg/middleware/headers.go
+++ pkg/middleware/headers.go
@@ -43,6 +43,7 @@ func newStripHeaders(headers []options.Header) alice.Constructor {
 
 func flattenHeaders(headers http.Header) {
 	for name, values := range headers {
+		// Set-Cookie should not be flattened, ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
 		if len(values) > 1 && name != "Set-Cookie" {
 			headers.Set(name, strings.Join(values, ","))
 		}
