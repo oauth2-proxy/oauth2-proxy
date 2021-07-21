f591b2174a8d8670e6184fb9bfd4d41a80a08116
Joshua Vécsei <git@vecsei.me>
2021-07-21 12:54:25 +0200

    switched to github.com/golang-jwt/jwt and updated golang.org/x/crypto to include CVE fixes

--- providers/logingov.go
+++ providers/logingov.go
@@ -10,7 +10,7 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/requests"
 	"gopkg.in/square/go-jose.v2"
