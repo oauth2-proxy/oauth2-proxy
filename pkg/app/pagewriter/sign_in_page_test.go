7c513481af1ad6d7d619770bd830bb513b6897fe
Miks Kalnins <MiksKalnins@maikumori.com>
2021-08-19 00:02:43 +0000

    Allow specifying URL as input for custom logos

--- pkg/app/pagewriter/sign_in_page_test.go
+++ pkg/app/pagewriter/sign_in_page_test.go
@@ -127,6 +127,16 @@ var _ = Describe("SignIn Page", func() {
 				expectedErr:  nil,
 				expectedData: "",
 			}),
+			Entry("with HTTP URL", loadCustomLogoTableInput{
+				logoPath:     "http://raw.githubusercontent.com/oauth2-proxy/oauth2-proxy/master/docs/static/img/logos/OAuth2_Proxy_icon.png",
+				expectedErr:  nil,
+				expectedData: "<img src=\"http://raw.githubusercontent.com/oauth2-proxy/oauth2-proxy/master/docs/static/img/logos/OAuth2_Proxy_icon.png\" alt=\"Logo\" />",
+			}),
+			Entry("with HTTPS URL", loadCustomLogoTableInput{
+				logoPath:     "https://raw.githubusercontent.com/oauth2-proxy/oauth2-proxy/master/docs/static/img/logos/OAuth2_Proxy_icon.png",
+				expectedErr:  nil,
+				expectedData: "<img src=\"https://raw.githubusercontent.com/oauth2-proxy/oauth2-proxy/master/docs/static/img/logos/OAuth2_Proxy_icon.png\" alt=\"Logo\" />",
+			}),
 			Entry("with an svg custom logo", loadCustomLogoTableInput{
 				logoPath:     "customDir/logo.svg",
 				expectedErr:  nil,
