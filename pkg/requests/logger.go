f6afd93a791808ffaef82c048bdab2f1a3e74c4b
Joel  Speed <joel.speed@hotmail.co.uk>
2021-10-06 17:29:17 +0100

    Fixup requests with verbose type

--- pkg/requests/logger.go
+++ pkg/requests/logger.go
@@ -2,10 +2,9 @@ package requests
 
 import (
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/logger"
-	"k8s.io/klog/v2"
 )
 
 var (
-	debugLogger = func() klog.Verbose { return klog.V(logger.RequestDebug) }
-	traceLogger = func() klog.Verbose { return klog.V(logger.RequestTrace) }
+	debugLogger = logger.Verbose(logger.RequestDebug)
+	traceLogger = logger.Verbose(logger.RequestTrace)
 )
