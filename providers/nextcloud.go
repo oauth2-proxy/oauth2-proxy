5cfcf2904ccb5da464f458ac473534ccb635c082
Max <870074+max-te@users.noreply.github.com>
2021-11-13 14:05:49 +0100

    [nextcloud] Switch nextcloud provider to EnrichSession

--- providers/nextcloud.go
+++ providers/nextcloud.go
@@ -1,6 +1,13 @@
 package providers
 
-import "github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/options"
+import (
+	"context"
+	"fmt"
+
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/options"
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/requests"
+)
 
 // NextcloudProvider represents an Nextcloud based Identity Provider
 type NextcloudProvider struct {
@@ -21,4 +28,41 @@ func NewNextcloudProvider(p *ProviderData) *NextcloudProvider {
 		p.EmailClaim = "ocs.data.email"
 	}
 	return &NextcloudProvider{ProviderData: p}
+}
+
+// EnrichSession is called after Redeem to allow providers to enrich session fields
+// such as User, Email, Groups with provider specific API calls.
+func (p *NextcloudProvider) EnrichSession(ctx context.Context, s *sessions.SessionState) error {
+	json, err := requests.New(p.ValidateURL.String()).
+		WithContext(ctx).
+		WithHeaders(makeOIDCHeader(s.AccessToken)).
+		Do().
+		UnmarshalJSON()
+	if err != nil {
+		return fmt.Errorf("error making request: %v", err)
+	}
+
+	email, err := json.GetPath("ocs", "data", "email").String()
+	if err != nil {
+		return fmt.Errorf("error retrieving email address: %v", err)
+	}
+	if email != "" {
+		s.Email = email
+	}
+
+	username, err := json.GetPath("ocs", "data", "id").String()
+	if err == nil && username != "" {
+		s.User = username
+	}
+
+	groups, err := json.GetPath("ocs", "data", "groups").StringArray()
+	if err == nil {
+		for _, group := range groups {
+			if group != "" {
+				s.Groups = append(s.Groups, group)
+			}
+		}
+	}
+
+	return nil
 }
