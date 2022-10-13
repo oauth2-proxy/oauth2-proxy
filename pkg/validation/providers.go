d8af34fe19761cbcb32bc53bdf2b8de69d45c75f
Jake Rote <jrote1@users.noreply.github.com>
2022-10-13 10:52:40 +0100

    Added support for public clients
    Adds support for public clients #1714

--- pkg/validation/providers.go
+++ pkg/validation/providers.go
@@ -49,7 +49,7 @@ func validateProvider(provider options.Provider, providerIDs map[string]struct{}
 	// login.gov uses a signed JWT to authenticate, not a client-secret
 	if provider.Type != "login.gov" {
 		if provider.ClientSecret == "" && provider.ClientSecretFile == "" {
-			msgs = append(msgs, "missing setting: client-secret or client-secret-file")
+			msgs = append(msgs, "missing setting: client-secret or client-secret-file" && provider.Type != "oidc")
 		}
 		if provider.ClientSecret == "" && provider.ClientSecretFile != "" {
 			_, err := os.ReadFile(provider.ClientSecretFile)
