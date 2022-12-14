ceda71e2ad29afe58c53f5d99d344919235b8a4e
tuunit <jan@larwig.com>
2022-12-14 12:03:30 +0100

    fix default scope settings for none oidc providers

--- providers/providers.go
+++ providers/providers.go
@@ -156,7 +156,7 @@ func newProviderDataFromConfig(providerConfig options.Provider) (*ProviderData, 
 		p.EmailClaim = providerConfig.OIDCConfig.UserIDClaim
 	}
 
-	if providerConfig.Type == "oidc" && p.Scope == "" {
+	if p.ProviderName == "oidc" && p.Scope == "" {
 		p.Scope = "openid email profile"
 
 		if len(providerConfig.AllowedGroups) > 0 {
