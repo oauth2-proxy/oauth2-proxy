92461ccd4e301f0370a3b8d4a41be6017579dd4b
aria1991 <47612848+aria1991@users.noreply.github.com>
2023-02-08 20:16:42 +0100

    Update dist.sh
    I added default values for BINARY and VERSION using the := operator, so that the script will still run if these environment variables are not set, and I made some modification to make the script easier to read and improve the error message for missing environment variables.

--- dist.sh
+++ dist.sh
@@ -2,38 +2,32 @@
 
 set -o errexit
 
-if [[ -z ${BINARY} ]] || [[ -z ${VERSION} ]]; then
-	echo "Missing required env var: BINARY=X VERSION=X $(basename $0)"
-	exit 1
+BINARY=${BINARY:-}
+VERSION=${VERSION:-}
+
+if [[ -z "$BINARY" ]] || [[ -z "$VERSION" ]]; then
+  echo "Missing required environment variable: BINARY and VERSION"
+  echo "Usage: BINARY=<binary-name> VERSION=<version-number> $0"
+  exit 1
 fi
 
 ARCHS=(darwin-amd64 linux-amd64 linux-arm64 linux-ppc64le linux-armv6 freebsd-amd64 windows-amd64)
+RELEASE_DIR="release"
+mkdir -p "$RELEASE_DIR"
 
-mkdir -p release
-
-# Create architecture specific release dirs
 for ARCH in "${ARCHS[@]}"; do
-	mkdir -p release/${BINARY}-${VERSION}.${ARCH}
+  ARCH_DIR="$RELEASE_DIR/${BINARY}-${VERSION}.${ARCH}"
+  mkdir -p "$ARCH_DIR"
 
-	GO_OS=$(echo $ARCH | awk -F- '{print $1}')
-	GO_ARCH=$(echo $ARCH | awk -F- '{print $2}')
+  GO_OS=$(echo "$ARCH" | awk -F- '{print $1}')
+  GO_ARCH=$(echo "$ARCH" | awk -F- '{print $2}')
 
-	# Create architecture specific binaries
-	if [[ ${GO_ARCH} == "armv6" ]]; then
-		GO111MODULE=on GOOS=${GO_OS} GOARCH=arm GOARM=6 CGO_ENABLED=0 go build -ldflags="-X main.VERSION=${VERSION}" \
-			-o release/${BINARY}-${VERSION}.${ARCH}/${BINARY} .
-	else
-		GO111MODULE=on GOOS=${GO_OS} GOARCH=${GO_ARCH} CGO_ENABLED=0 go build -ldflags="-X main.VERSION=${VERSION}" \
-			-o release/${BINARY}-${VERSION}.${ARCH}/${BINARY} .
-	fi
+  CGO_ENABLED=0 GO111MODULE=on GOOS="$GO_OS" GOARCH="$GO_ARCH" \
+    go build -ldflags="-X main.VERSION=$VERSION" \
+    -o "$ARCH_DIR/$BINARY" .
 
-	cd release
-
-	# Create sha256sum for architecture specific binary
-	sha256sum ${BINARY}-${VERSION}.${ARCH}/${BINARY} > ${BINARY}-${VERSION}.${ARCH}-sha256sum.txt
-
-	# Create tar file for architecture specific binary
-	tar -czvf ${BINARY}-${VERSION}.${ARCH}.tar.gz ${BINARY}-${VERSION}.${ARCH}
-
-	cd ..
+  cd "$RELEASE_DIR"
+  sha256sum "${BINARY}-${VERSION}.${ARCH}/$BINARY" > "${BINARY}-${VERSION}.${ARCH}-sha256sum.txt"
+  tar -czvf "${BINARY}-${VERSION}.${ARCH}.tar.gz" "${BINARY}-${VERSION}.${ARCH}"
+  cd ..
 done
