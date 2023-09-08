4bb4f32177ca5b652d6c48b44e326c4e871e77e7
tuunit <jan@larwig.com>
2023-09-08 21:37:16 +0000

    refactor documentation workflow to follow best github action best practices

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -59,13 +59,7 @@ module.exports = {
       require.resolve("@easyops-cn/docusaurus-search-local"),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        // language: ["en", "zh"],
-        // ```
       }),
     ],
   ],
