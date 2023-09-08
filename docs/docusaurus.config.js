58dfe37c77893d72aeb7b6fa07199b66913ff202
tuunit <jan@larwig.com>
2023-09-08 21:36:45 +0000

    add search and update depedencies

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -54,4 +54,19 @@ module.exports = {
       },
     ],
   ],
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
+    ],
+  ],
 };
