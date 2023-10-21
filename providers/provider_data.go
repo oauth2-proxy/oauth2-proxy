f4df378a6f05e50a70689410af5d834a7cd50336
TommyLike <tommylikehu@gmail.com>
2023-10-21 11:03:07 +0800

    Rename prefered username to username when get info from claim

--- providers/provider_data.go
+++ providers/provider_data.go
@@ -256,7 +256,8 @@ func (p *ProviderData) buildSessionFromClaims(rawIDToken, accessToken string) (*
 		{p.EmailClaim, &ss.Email},
 		{p.GroupsClaim, &ss.Groups},
 		// TODO (@NickMeves) Deprecate for dynamic claim to session mapping
-		{"preferred_username", &ss.PreferredUsername},
+		// NOTE(tommylike): for our ID server we decide to use "username" as our preferred_username source.
+		{"username", &ss.PreferredUsername},
 	} {
 		if _, err := extractor.GetClaimInto(c.claim, c.dst); err != nil {
 			return nil, err
