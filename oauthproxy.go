f32d78fe93a7e598ec0b4e13471b1dbeef2bddc6
Nuno Borges (CTW) <Nuno.Borges@ctw.bmwgroup.com>
2023-11-18 17:47:46 +0000

    Issue 2311: runtime error: index out of range [0] with length 0 while extracting state of the csrf

--- oauthproxy.go
+++ oauthproxy.go
@@ -800,13 +800,10 @@ func (p *OAuthProxy) doOAuthStart(rw http.ResponseWriter, req *http.Request, ove
 		logger.Errorf("Error setting CSRF cookie: %v", err)
 		p.ErrorPage(rw, req, http.StatusInternalServerError, err.Error())
 		return
-	} else {
-		// There are a lot of issues opened complaining about missing CSRF cookies.
-		// The reasons can be several: network policies, bad configuration, or may even be a from OAuthProxy.
-		// Do some logging to show what CSRF cookies were sent.
-		// Try to log the INs and OUTs of OAuthProxy, to be easier to analyse these issues.
-		logger.Println("CSRF cookie %s was sent in request (state=%s).", csrfCookie.Name, hashOAuthState)
 	}
+	// There are a lot of issues opened complaining about missing CSRF cookies.
+	// Try to log the INs and OUTs of OAuthProxy, to be easier to analyse these issues.
+	logger.Println("CSRF cookie %s was sent in request (state=%s).", csrfCookie.Name, hashOAuthState)
 
 	http.Redirect(rw, req, loginURL, http.StatusFound)
 }
@@ -846,10 +843,8 @@ func (p *OAuthProxy) OAuthCallback(rw http.ResponseWriter, req *http.Request) {
 	csrf, err := cookies.LoadCSRFCookie(req, cookieName, p.CookieOptions)
 	if err != nil {
 		// There are a lot of issues opened complaining about missing CSRF cookies.
-		// The reasons can be several: network policies, bad configuration, or may even be a from OAuthProxy.
-		// Do some logging to show what CSRF cookies were received in OAuth callback.
 		// Try to log the INs and OUTs of OAuthProxy, to be easier to analyse these issues.
-		LoggingCSRFCookiesInOAuthCallback(req, cookieName)
+		cookies.LoggingCSRFCookiesInOAuthCallback(req, cookieName)
 		logger.Println(req, logger.AuthFailure, "Invalid authentication via OAuth2: unable to obtain CSRF cookie: %s (state=%s)", err, nonce)
 		p.ErrorPage(rw, req, http.StatusForbidden, err.Error(), "Login Failed: Unable to find a valid CSRF token. Please try again.")
 		return
@@ -1257,27 +1252,4 @@ func (p *OAuthProxy) errorJSON(rw http.ResponseWriter, code int) {
 	// we need to send some JSON response because we set the Content-Type to
 	// application/json
 	rw.Write([]byte("{}"))
-}
-
-// LoggingCSRFCookiesInOAuthCallback Log all CSRF cookies found in HTTP request OAuth callback, which were successfully parsed
-func LoggingCSRFCookiesInOAuthCallback(req *http.Request, cookieName string) {
-	cookies := req.Cookies()
-	if len(cookies) == 0 {
-		logger.Println(req, logger.AuthFailure, "No cookies were found in OAuth callback.")
-	} else {
-		var anyFound = false
-		for i := range cookies {
-			var c = cookies[i]
-			if cookieName == c.Name {
-				logger.Println(req, logger.AuthFailure, "The CSRF cookie %s was found in OAuth callback.", c.Name)
-				anyFound = true
-			} else if strings.Contains(c.Name, "_csrf") {
-				logger.Println(req, logger.AuthFailure, "The CSRF cookie %s was found in OAuth callback, but it is not the expected one (%s).", c.Name, cookieName)
-				anyFound = true
-			}
-		}
-		if !anyFound {
-			logger.Println(req, logger.AuthFailure, "Cookies were found in OAuth callback, but none was a CSRF cookie.")
-		}
-	}
 }
