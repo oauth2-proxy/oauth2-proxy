b6da03604981fec57adbaf37e7e7d16e6cddd6a8
Jacob Middag <jacob@gaddim.nl>
2024-03-27 09:08:47 +0100

    Make userAgentTransport configurable and composable

--- pkg/requests/http.go
+++ pkg/requests/http.go
@@ -7,18 +7,23 @@ import (
 )
 
 type userAgentTransport struct {
+	next      http.RoundTripper
+	userAgent string
 }
 
 func (t *userAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	r2 := req.Clone(req.Context())
-	setDefaultUserAgent(r2.Header)
-	return http.DefaultTransport.RoundTrip(r2)
+	setDefaultUserAgent(r2.Header, t.userAgent)
+	return t.next.RoundTrip(r2)
 }
 
-var DefaultHttpClient = &http.Client{Transport: &userAgentTransport{}}
+var DefaultHttpClient = &http.Client{Transport: &userAgentTransport{
+	http.DefaultTransport,
+	"oauth2-proxy/" + pkg.VERSION,
+}}
 
-func setDefaultUserAgent(header http.Header) {
+func setDefaultUserAgent(header http.Header, userAgent string) {
 	if header != nil && len(header.Values("User-Agent")) == 0 {
-		header.Set("User-Agent", "oauth2-proxy/"+pkg.VERSION)
+		header.Set("User-Agent", userAgent)
 	}
 }
