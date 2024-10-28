837122ea6c505a4a606d4f1b1017337e5d4a15c5
tuunit <jan@larwig.com>
2024-10-28 20:35:23 +0100

    fix test case and reformat

--- providers/ms_entra_id_test.go
+++ providers/ms_entra_id_test.go
@@ -40,6 +40,92 @@ func TestAzureEntraOIDCProviderNewSingleTenant(t *testing.T) {
 	g.Expect(provider.Data().ProviderName).To(Equal("Microsoft Entra ID"))
 }
 
+func TestAzureEntraOIDCProviderEnrichSessionGroupOverage(t *testing.T) {
+	// Create ID Token that indicates group overage with _claim_names
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	claimsWithGroupOverage := &claimsWithGroupOverage{
+		jwt.RegisteredClaims{
+			Issuer: "https://login.microsoftonline.com/18014347-dd57-41a1-8191-7a1f734ea457/v2.0",
+		},
+		map[string]string{"groups": "src1"},
+	}
+
+	jwtWithClaims := jwt.NewWithClaims(jwt.SigningMethodRS256, claimsWithGroupOverage)
+	signedJWT, err := jwtWithClaims.SignedString(key)
+
+	assert.NoError(t, err)
+
+	session := CreateAuthorizedSession()
+	session.IDToken = signedJWT
+	session.Email = "mock@example.com"
+
+	// Create provider
+	provider := NewMicrosoftEntraIDProvider(&ProviderData{},
+		options.Provider{OIDCConfig: options.OIDCOptions{
+			IssuerURL: "https://login.microsoftonline.com/18014347-dd57-41a1-8191-7a1f734ea457/v2.0",
+		}},
+	)
+
+	// Create mocked Azure Graph server and override Graph URL
+	mockedGraph := mockGraphAPI(false)
+	mockedGraphURL, _ := url.Parse(mockedGraph.URL)
+	updateURL(provider.microsoftGraphURL, mockedGraphURL.Host)
+
+	// Test EnrichSession
+	err = provider.EnrichSession(context.Background(), session)
+
+	assert.NoError(t, err)
+	assert.Contains(t, session.Groups, "85d7d600-7804-4d92-8d43-9c33c21c130c")
+	assert.Contains(t, session.Groups, "916f0604-8a3b-4a69-bda9-06db11a8f0cd")
+}
+
+func TestAzureEntraOIDCProviderValidateSessionAllowedTenants(t *testing.T) {
+	// Create multi-tenant Azure Entra provider with allowed tenants
+	provider := NewMicrosoftEntraIDProvider(
+		&ProviderData{
+			Verifier: &mockedVerifier{},
+		},
+		options.Provider{
+			OIDCConfig: options.OIDCOptions{
+				IssuerURL:                      "https://login.microsoftonline.com/common/v2.0",
+				InsecureSkipIssuerVerification: true,
+				InsecureSkipNonce:              true,
+			},
+			MicrosoftEntraIDConfig: options.MicrosoftEntraIDOptions{
+				AllowedTenants: []string{"85d7d600-7804-4d92-8d43-9c33c21c130c"},
+			},
+		},
+	)
+
+	// Check for invalid tenant
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	idToken := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.RegisteredClaims{
+		Issuer: "https://login.microsoftonline.com/invalid_tenant/v2.0",
+	})
+	invalidJWT, err := idToken.SignedString(key)
+	assert.NoError(t, err)
+
+	session := CreateAuthorizedSession()
+	session.IDToken = invalidJWT
+
+	valid := provider.ValidateSession(context.Background(), session)
+	assert.False(t, valid)
+
+	// Check for valid tenant
+	idToken = jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.RegisteredClaims{
+		Issuer: "https://login.microsoftonline.com/85d7d600-7804-4d92-8d43-9c33c21c130c/v2.0",
+	})
+	validJWT, err := idToken.SignedString(key)
+	assert.NoError(t, err)
+
+	session = CreateAuthorizedSession()
+	session.IDToken = validJWT
+
+	valid = provider.ValidateSession(context.Background(), session)
+	assert.True(t, valid)
+}
+
 func mockGraphAPI(noGroupMemberPermissions bool) *httptest.Server {
 	groupsPath := "/v1.0/me/transitiveMemberOf"
 
@@ -75,97 +161,9 @@ func (c *claimsWithGroupOverage) Valid() error {
 	return nil
 }
 
-func TestAzureEntraOIDCProviderEnrichSessionGroupOverage(t *testing.T) {
-
-	// Create ID Token that indicates group overage with _claim_names
-	key, _ := rsa.GenerateKey(rand.Reader, 2048)
-	claimsWithGroupOverage := &claimsWithGroupOverage{
-		jwt.RegisteredClaims{
-			Issuer: "https://login.microsoftonline.com/18014347-dd57-41a1-8191-7a1f734ea457/v2.0",
-		},
-		map[string]string{"groups": "src1"},
-	}
-
-	jwtWithClaims := jwt.NewWithClaims(jwt.SigningMethodRS256, claimsWithGroupOverage)
-	signedJWT, err := jwtWithClaims.SignedString(key)
-
-	assert.NoError(t, err)
-
-	session := CreateAuthorizedSession()
-	session.IDToken = signedJWT
-
-	// Create provider
-	provider := NewMicrosoftEntraIDProvider(&ProviderData{},
-		options.Provider{OIDCConfig: options.OIDCOptions{
-			IssuerURL: "https://login.microsoftonline.com/18014347-dd57-41a1-8191-7a1f734ea457/v2.0",
-		}},
-	)
-
-	// Create mocked Azure Graph server and override Graph URL
-	mockedGraph := mockGraphAPI(false)
-	mockedGraphURL, _ := url.Parse(mockedGraph.URL)
-	updateURL(provider.microsoftGraphURL, mockedGraphURL.Host)
-
-	// Test EnrichSession
-	err = provider.EnrichSession(context.Background(), session)
-
-	assert.NoError(t, err)
-	assert.Contains(t, session.Groups, "85d7d600-7804-4d92-8d43-9c33c21c130c")
-	assert.Contains(t, session.Groups, "916f0604-8a3b-4a69-bda9-06db11a8f0cd")
-
-}
-
 type mockedVerifier struct {
 }
 
 func (v *mockedVerifier) Verify(ctx context.Context, rawIDToken string) (*oidc.IDToken, error) {
 	return nil, nil
-}
-
-func TestAzureEntraOIDCProviderValidateSessionAllowedTenants(t *testing.T) {
-	// Create multi-tenant Azure Entra provider with allowed tenants
-	provider := NewMicrosoftEntraIDProvider(
-		&ProviderData{
-			Verifier: &mockedVerifier{},
-		},
-		options.Provider{
-			OIDCConfig: options.OIDCOptions{
-				IssuerURL:                      "https://login.microsoftonline.com/common/v2.0",
-				InsecureSkipIssuerVerification: true,
-				InsecureSkipNonce:              true,
-			},
-			MicrosoftEntraIDConfig: options.MicrosoftEntraIDOptions{
-				AllowedMultiTenants: []string{"85d7d600-7804-4d92-8d43-9c33c21c130c"},
-			},
-		},
-	)
-
-	// Check for invalid tenant
-	key, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	idToken := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.RegisteredClaims{
-		Issuer: "https://login.microsoftonline.com/invalid_tenant/v2.0",
-	})
-	invalidJWT, err := idToken.SignedString(key)
-	assert.NoError(t, err)
-
-	session := CreateAuthorizedSession()
-	session.IDToken = invalidJWT
-
-	valid := provider.ValidateSession(context.Background(), session)
-	assert.False(t, valid)
-
-	// Check for valid tenant
-	idToken = jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.RegisteredClaims{
-		Issuer: "https://login.microsoftonline.com/85d7d600-7804-4d92-8d43-9c33c21c130c/v2.0",
-	})
-	validJWT, err := idToken.SignedString(key)
-	assert.NoError(t, err)
-
-	session = CreateAuthorizedSession()
-	session.IDToken = validJWT
-
-	valid = provider.ValidateSession(context.Background(), session)
-	assert.True(t, valid)
-
 }
