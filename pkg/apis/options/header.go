a26e8c150aa94f30df098c99bdd8abeb55cefddf
tuunit <jan@larwig.com>
2025-02-09 17:23:03 +0100

    use official upstream yaml library v3

--- pkg/apis/options/header.go
+++ pkg/apis/options/header.go
@@ -5,26 +5,26 @@ package options
 type Header struct {
 	// Name is the header name to be used for this set of values.
 	// Names should be unique within a list of Headers.
-	Name string `json:"name,omitempty"`
+	Name string `yaml:"name,omitempty"`
 
 	// PreserveRequestValue determines whether any values for this header
 	// should be preserved for the request to the upstream server.
 	// This option only applies to injected request headers.
 	// Defaults to false (headers that match this header will be stripped).
-	PreserveRequestValue bool `json:"preserveRequestValue,omitempty"`
+	PreserveRequestValue bool `yaml:"preserveRequestValue,omitempty"`
 
 	// Values contains the desired values for this header
-	Values []HeaderValue `json:"values,omitempty"`
+	Values []HeaderValue `yaml:"values,omitempty"`
 }
 
 // HeaderValue represents a single header value and the sources that can
 // make up the header value
 type HeaderValue struct {
 	// Allow users to load the value from a secret source
-	*SecretSource `json:"secretSource,omitempty"`
+	*SecretSource `yaml:"secretSource,omitempty"`
 
 	// Allow users to load the value from a session claim
-	*ClaimSource `json:"claimSource,omitempty"`
+	*ClaimSource `yaml:"claimSource,omitempty"`
 }
 
 // ClaimSource allows loading a header value from a claim within the session
@@ -32,14 +32,14 @@ type ClaimSource struct {
 	// Claim is the name of the claim in the session that the value should be
 	// loaded from. Available claims: `access_token` `id_token` `created_at`
 	// `expires_on` `refresh_token` `email` `user` `groups` `preferred_username`.
-	Claim string `json:"claim,omitempty"`
+	Claim string `yaml:"claim,omitempty"`
 
 	// Prefix is an optional prefix that will be prepended to the value of the
 	// claim if it is non-empty.
-	Prefix string `json:"prefix,omitempty"`
+	Prefix string `yaml:"prefix,omitempty"`
 
 	// BasicAuthPassword converts this claim into a basic auth header.
 	// Note the value of claim will become the basic auth username and the
 	// basicAuthPassword will be used as the password value.
-	BasicAuthPassword *SecretSource `json:"basicAuthPassword,omitempty"`
+	BasicAuthPassword *SecretSource `yaml:"basicAuthPassword,omitempty"`
 }
--- pkg/apis/options/secret_source.go
+++ pkg/apis/options/secret_source.go
@@ -4,11 +4,11 @@ package options
 // Only one source within the struct should be defined at any time.
 type SecretSource struct {
 	// Value expects a base64 encoded string value.
-	Value string `json:"value,omitempty"`
+	Value string `yaml:"value,omitempty"`
 
 	// FromEnv expects the name of an environment variable.
-	FromEnv string `json:"fromEnv,omitempty"`
+	FromEnv string `yaml:"fromEnv,omitempty"`
 
 	// FromFile expects a path to a file containing the secret value.
-	FromFile string `json:"fromFile,omitempty"`
+	FromFile string `yaml:"fromFile,omitempty"`
 }
