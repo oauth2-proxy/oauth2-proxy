tottalynotawebsitefornonschoolthings-35/william

Return CSRF token as cookie (#2573)
Introduce helpers that will wrap the CSRF token in a cookie. Set the
cookie along with the session cookie, when the callback function
is called. Expose the following flags to configure CSRF token cookie
attributes:

* --csrftoken-cookie-name: the name of the CSRF token cookie (default:
  _oauth2_proxy_csrftoken). If set to an empty string, the functionality
  is disabled.
* --csrftoken-cookie-domain: the domain(s) of the cookie (optional, default "")
* --csrftoken-cookie-path: the path of the cookie (optional, default: /)
* --csrftoken-cookie-expire: expiration of the cookie (default: 168 hours)
* --csrftoken-cookie-secure: set secure (HTTPS) cookie (default: true)
* --csrftoken-cookie-httponly: set HTTPOnly for cookie (default: false)
* --csrftoken-cookie-samesite: set SameSite for cookie (default: strict)

Note that a CSRF cookie should be secure but not HttpOnly, since clients
are expected to retrieve the value of the token to send in subsequent
requests.

Set SameSite to strict by default.

Co-authored-by: Athina Plaskasoviti <athina.plaskasoviti@hpe.com>
Co-authored-by: Konstantinos Lolos <konstantinos.lolos@hpe.com>

pkg/apis/options/csrftoken.go
@@ -1,22 +1,45 @@
 package options
 
 import (
+	"time"
+
 	"github.com/spf13/pflag"
 )
 
 type CSRFToken struct {
-	CSRFToken bool `flag:"csrftoken" cfg:"csrftoken"`
+	CSRFToken      bool          `flag:"csrftoken" cfg:"csrftoken"`
+	CookieName     string        `flag:"csrftoken-cookie-name" cfg:"csrftoken_cookie_name"`
+	CookieDomains  []string      `flag:"csrftoken-cookie-domain" cfg:"csrftoken_cookie_domain"`
+	CookiePath     string        `flag:"csrftoken-cookie-path" cfg:"csrftoken_cookie_path"`
+	CookieExpire   time.Duration `flag:"csrftoken-cookie-expire" cfg:"csrftoken_cookie_expire"`
+	CookieSecure   bool          `flag:"csrftoken-cookie-secure" cfg:"csrftoken_cookie_secure"`
+	CookieHTTPOnly bool          `flag:"csrftoken-cookie-httponly" cfg:"csrftoken_cookie_httponly"`
+	CookieSameSite string        `flag:"csrftoken-cookie-samesite" cfg:"csrftoken_cookie_samesite"`
 }
 
 func csrfTokenFlagSet() *pflag.FlagSet {
 	flagSet := pflag.NewFlagSet("csrftoken", pflag.ExitOnError)
 
 	flagSet.Bool("csrftoken", false, "enable the CSRF token generation")
+	flagSet.String("csrftoken-cookie-name", "_oauth2_proxy_csrftoken", "the name of the CSRF token cookie that the oauth2-proxy creates. If set to empty string, so CSRF cookie will be created for the session.")
+	flagSet.StringSlice("csrftoken-cookie-domain", []string{}, "the domain(s) of the CSRF token cookie (ie: `.yourcompany.com`). The longest domain matching the request's host will be used (or the shortest cookie domain if there is no match).")
+	flagSet.String("csrftoken-cookie-path", "/", "the path of the CSRF token cookie (ie: /poc/)*")
+	flagSet.Duration("csrftoken-cookie-expire", time.Duration(168)*time.Hour, "expiration timeframe for the CSRF token cookie")
+	flagSet.Bool("csrftoken-cookie-secure", true, "set secure (HTTPS) cookie flag for CSRF token cookie")
+	flagSet.Bool("csrftoken-cookie-httponly", false, "set HttpOnly cookie flag for CSRF token cookie")
+	flagSet.String("csrftoken-cookie-samesite", "strict", "set SameSite cookie attribute for CSRF token cookie (ie: \"lax\", \"strict\", \"none\", or \"\"). ")
 	return flagSet
 }
 
 func CSRFTokenDefaults() CSRFToken {
 	return CSRFToken{
-		CSRFToken: false,
+		CSRFToken:      false,
+		CookieName:     "_oauth2_proxy_csrftoken",
+		CookieDomains:  nil,
+		CookiePath:     "/",
+		CookieExpire:   time.Duration(168) * time.Hour,
+		CookieSecure:   true,
+		CookieHTTPOnly: false,
+		CookieSameSite: "strict",
 	}
 }

oauth2-proxy/oauth2-proxy

pkg/apis/options/csrftoken.go
@@ -1,22 +1,45 @@
 package options
 
 import (
+	"time"
+
 	"github.com/spf13/pflag"
 )
 
 type CSRFToken struct {
-	CSRFToken bool `flag:"csrftoken" cfg:"csrftoken"`
+	CSRFToken      bool          `flag:"csrftoken" cfg:"csrftoken"`
+	CookieName     string        `flag:"csrftoken-cookie-name" cfg:"csrftoken_cookie_name"`
+	CookieDomains  []string      `flag:"csrftoken-cookie-domain" cfg:"csrftoken_cookie_domain"`
+	CookiePath     string        `flag:"csrftoken-cookie-path" cfg:"csrftoken_cookie_path"`
+	CookieExpire   time.Duration `flag:"csrftoken-cookie-expire" cfg:"csrftoken_cookie_expire"`
+	CookieSecure   bool          `flag:"csrftoken-cookie-secure" cfg:"csrftoken_cookie_secure"`
+	CookieHTTPOnly bool          `flag:"csrftoken-cookie-httponly" cfg:"csrftoken_cookie_httponly"`
+	CookieSameSite string        `flag:"csrftoken-cookie-samesite" cfg:"csrftoken_cookie_samesite"`
 }
 
 func csrfTokenFlagSet() *pflag.FlagSet {
 	flagSet := pflag.NewFlagSet("csrftoken", pflag.ExitOnError)
 
 	flagSet.Bool("csrftoken", false, "enable the CSRF token generation")
+	flagSet.String("csrftoken-cookie-name", "_oauth2_proxy_csrftoken", "the name of the CSRF token cookie that the oauth2-proxy creates. If set to empty string, no CSRF cookie will be created for the session.")
+	flagSet.StringSlice("csrftoken-cookie-domain", []string{}, "the domain(s) of the CSRF token cookie (ie: `.yourcompany.com`). The longest domain matching the request's host will be used (or the shortest cookie domain if there is no match).")
+	flagSet.String("csrftoken-cookie-path", "/", "the path of the CSRF token cookie (ie: /poc/)*")
+	flagSet.Duration("csrftoken-cookie-expire", time.Duration(168)*time.Hour, "expiration timeframe for the CSRF token cookie")
+	flagSet.Bool("csrftoken-cookie-secure", true, "set secure (HTTPS) cookie flag for CSRF token cookie")
+	flagSet.Bool("csrftoken-cookie-httponly", false, "set HttpOnly cookie flag for CSRF token cookie")
+	flagSet.String("csrftoken-cookie-samesite", "strict", "set SameSite cookie attribute for CSRF token cookie (ie: \"lax\", \"strict\", \"none\", or \"\"). ")
 	return flagSet
 }
 
 func CSRFTokenDefaults() CSRFToken {
 	return CSRFToken{
-		CSRFToken: false,
+		CSRFToken:      false,
+		CookieName:     "_oauth2_proxy_csrftoken",
+		CookieDomains:  nil,
+		CookiePath:     "/",
+		CookieExpire:   time.Duration(168) * time.Hour,
+		CookieSecure:   true,
+		CookieHTTPOnly: false,
+		CookieSameSite: "strict",
 	}
 }