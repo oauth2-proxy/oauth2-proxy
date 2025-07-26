d2f72ef5ef255e46267c6fced3868e591b38ff9a
Jan Larwig <jan@larwig.com>
2025-07-26 12:46:17 +0200

    fix test setup and add local image build make target
    Signed-off-by: Jan Larwig <jan@larwig.com>

--- pkg/apis/options/secret_source.go
+++ pkg/apis/options/secret_source.go
@@ -3,7 +3,7 @@ package options
 // SecretSource references an individual secret value.
 // Only one source within the struct should be defined at any time.
 type SecretSource struct {
-	// Value expects a base64 encoded []byte
+	// Value expects a base64 encoded string value.
 	Value []byte `yaml:"value,omitempty"`
 
 	// FromEnv expects the name of an environment variable.
